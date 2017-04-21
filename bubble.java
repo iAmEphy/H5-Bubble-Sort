ee0af72f973ca45b1e7b4e382d8e8a577873f538
xyuzuki <lindaliu@Yuzuki.local>
2017-04-20 23:57:40 -0400

    Constructors/nodes
    Setup

--- bubble.java
+++ bubble.java
@@ -1,5 +1,29 @@
 public class bubble
 {
+  
+  int value;
+  Node next;
+  
+  public Node()
+  {
+    value = 0;
+    next = null;
+  }
+  
+  public Node(int value)
+  {
+    this.value = value;
+    next = null;
+  }
+  
+  public Node(int value, Node next)
+  {
+    this.value = next;
+    this.next = next;
+  }
+  
+  
+  
   public static void main(String[] args)
   {
   }
